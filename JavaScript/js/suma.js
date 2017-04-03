970b26b3fac3064c8d8e30307d81e26a58cd4456
jadr94 <jadr_94@hotmail.com>
2017-04-03 15:04:13 -0600

    Ejemplo01 javaScirpt

---
+++ JavaScript/js/suma.js
@@ -0,0 +1,17 @@
+function texto(){
+  alert(document.getElementById("texto1").value)
+}
+function suma(a+b){
+    return a+b;
+  }
+  var suma2 = function(a+b){
+    return a+b;
+  }
+
+function suma3(){
+
+   var n1= alert(document.getElementById("a").value)
+   var n2= alert(document.getElementById("b").value)
+   return(parseInt(n1)+parseInt(n2))
+
+  }
