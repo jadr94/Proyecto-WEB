cdede1d68fafe8150cb3daaf7cd799536c11b992
jadr94 <jadr_94@hotmail.com>
2017-04-07 15:01:06 -0600

    Calculadora parte 3

--- calculadora/index.js
+++ calculadora/index.js
@@ -1,29 +1,47 @@
-//
-const rq = require('electron-require');
-const main = rq.remote('./main.js');
+//Agrega al codigo (un enlace) al main.js
+const rq= require('electron-require');
+const main= rq.remote('./main.js');
+var botonesNumero= ["btn00","btn01","btn02","btn03","btn04","btn05","btn06","btn07","btn08","btn09"];
 
-var botonesNumero=["btn00","btn01","btn02","btn03","btn04","btn05","btn06","btn07","btn08","btn09"]
-var botonesOperador=["btnSumar","btnResta","btnDivision","btnMultiplicacion"]
-
+var botonesOperador= ["btnSumar","btnRestar","btnMultiplicar","btnDividir"];
+var operador="";
 function numeros(){
-	alert(this.id);
-	alert(this.value);
+var num=String(this.value)
+ if (operador==""){ //validando el operador1
+ 	var va= document.getElementById("operador1").value;
+ 	if(va=="0"){
+ 		document.getElementById("operador1").value="";
+ 	}
+ 	document.getElementById("operador1").value+=num;
+ } else{
+ 	var va= document.getElementById("operador2").value;
+ 	if(va=="0"){
+ 		document.getElementById("operador2").value="";
+ 	}
+ 	document.getElementById("operador2").value+=num;
+ }
 }
 function operadores(){
-	
+ operador= String(this.value);//+,-,*,/
 }
 function igual(){
-	
+var valor1=document.getElementById("operador1").value;
+var valor2=document.getElementById("operador2").value;
+document.getElementById("resultado").value=eval(valor1+operador+valor2);
 }
 function borrar(){
-	
+	document.getElementById("operador1").value= "0";
+	document.getElementById("operador2").value= "0";
+	document.getElementById("resultado").value= "0";
+	operador="";
 }
-for (var i = 0;i<botonesNumero.length ; i++) {
-	document.getElementByID(botonesNumero[i]).addEventListener('click',numeros)
+//Asignacion de eventos a los botones de numeros
+for (var i=0;i<botonesOperador.length;i++){
+	document.getElementById(botonesOperador [i]).addEventListener('click',numeros);//checar
 }
-for (var i = 0;i<botonesOperador.length ; i++) {
-	document.getElementByID(botonesOperador[i]).addEventListener('click',operadores)
+//Asignacion de eventos para los botones de operador
+for (var i=0;i<botonesNumero.length;i++){
+	document.getElementById(botonesNumero [i]).addEventListener('click',Operadores);//checar
 }
-
-document.getElementByID("btnIgual").addEventListener('click',igual)
-document.getElementByID("btnCE").addEventListener('click',borrar)
+document.getElementById("btnIgual").addEventListener('click',igual)
+document.getElementById("btnBorrar").addEventListener('click',borrar)
--- calculadora/main.js
+++ calculadora/main.js
@@ -1,24 +1,26 @@
-//const app = require('electron').app
-//const BrowserWindow = require('electron').BrowserWindow
-//igual que lo de abajo pero resumido
-const{app,BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url') //ruta en formato url
-let PantallaPrincipal; 
+//const app= require('electron').app
+//const BrowserWindow= require('electron').BrowserWindow
+const {app,BrowserWindow} = require('electron')
+//Ruta donde se encuentra nuestro proyecto
+const path= require('path')
+//Ruta pero en formato URL
+const url= require('url')
+let PantallaPrincipal;
 
-function MuestraPantallaPrincipal() {
-	PantallaPrincipal = new BrowserWindow({
-		width: 1024, height:768
-	})
-	PantallaPrincipal.on("closed",function(){
-		PantallaPrincipal = null
-	})
-	PantallaPrincipal.loadURL(url.format({
-		pathname: path.join(__dirname,'index.html'),
-		protocol: 'file',
-		slashes: true
-	}))
-	PantallaPrincipal.show()
+function muestraPantallaPrincipal(){
+PantallaPrincipal= new BrowserWindow({
+	width: 335, 
+	height:440
+})
+PantallaPrincipal.on('closed', function(){
+	PantallaPrincipal=null
+})
+PantallaPrincipal.loadURL(url.format({
+	pathname: path.join(__dirname,'index.html'),
+	protocol: 'file',
+	slashes: true
+}))
+PantallaPrincipal.show()
 }
-
-app.on('ready',MuestraPantallaPrincipal)
+//La aplicacion ejecuta este evento cuando el archivo main.js se carga en memoria.
+app.on('ready', muestraPantallaPrincipal)
