a6553dacb66476e03648584dfb0d10414f653c06
jadr94 <jadr_94@hotmail.com>
2017-05-04 14:10:53 -0600

    marvel parte3

--- marvel2/index.js
+++ marvel2/index.js
@@ -1,44 +1,66 @@
-const rq  = require('electron-require');
-const main= rq.remote('./main.js');
-const $   = require ("jquery");
+const rq   = require('electron-require');
+const main = rq.remote('./main.js');
+const $    = require("jquery");
 
+//Constantes para imprimir en PDF (ventana que imprimimos)
+const ipc = require('electron').ipcRenderer
+const btnPDF = document.getElementById('btnPDF')
+btnPDF.addEventListener('click',function(event){
+	ipc.send('print-to-pdf')
+})
 var buscarPersonaje = function(){
-	var personaje = $("#txtpersonaje").val();
+	var personaje = $("#txtPersonaje").val();
 	var url="http://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith="
 	var cantidadComics = 0
-	var cadenaComics = ""
+	var cadenaComics   = ""
+
 	if(personaje == ""){
 		alert("Ingrese el personaje")
-		$("#txtpersonaje").focus();
-		return //ya no continua con la funcion
+		$("#txtPersonaje").focus();
+		return //nos salimos de la funcion buscarpersonaje y Ya no continua con la función 
 	}
-	url = url+personaje;
+	url=url+personaje;
 	$.ajax({
+		beforeSend: function(){
+			$("#imgLoader").show();
+		},
 		dataType: "json",
-		url: url,
-		success:function(response){
-			if(response.code==200){//ok.cat
-				$("#imgFoto").show("slow")
+		url: url, //queremos una url que esta en la variable url
+		success:function(response){ //response es nuestra respuesta json
+			$("#imgLoader").hide();
+			if(response.code==200){ //ok.cat
+				$("#imgFoto").show("slow");
 				$("#imgFoto").attr("src",response.data.results[0].thumbnail.path+"."+
-					                     response.data.results[0].thumbnail.extension)
-				$("#txtNombre").html(response.data.results[0].name)
-				$("#txtDescripcion").html("")
+										 response.data.results[0].thumbnail.extension);
+				//dejo de funcionar desde aqui
+				$("#txtNombre").html(response.data.results[0].name);
+				$("#txtDescription").html("");
+				//validamos si hay o no descripción del personaje
 				if(response.data.results[0].description!=""){
-					$("#txtDescripcion").html("<b>Descripción<b>"+response.data.results[0].description)
+					$("#txtDescription").html("<b>Descripción: <b>"+response.data.results[0].description+"<br>");
 				}
-				cantidadComics=response.data.results[0].comics.returned
-				for (var i = 0;i<cantidadComics;i++) {
-				 	cadenaComics+=response.data.results[0].comics.items[i].name.+
-				 "<br>"
+				cantidadComics=response.data.results[0].comics.returned;
+				for(var i=0; i<cantidadComics;i++){
+					cadenaComics+=response.data.results[0].comics.items[i].name+"<br>"
 				}
-				$("#txtComics").html(cadenaComics)
+				$("#txtComics").html("<b>Comics: <b><br>"+cadenaComics);
 
 			}
-
 		}
+
 	})
+	$("#txtPersonaje").val("");
+	$("#txtPersonaje").focus();
 }
-//Posicion el cursor en el cuadro de texto
-$("#txtpersonaje").focus();
+var teclaPersonaje = function(tecla){
+	//Enter = 10,13(avance de linea y retorno de carro)
+	if(tecla.which==13){
+		buscarPersonaje();
+	}
+}
+
+//Posiciona el cursos en el cuadro de texto
+$("#txtPersonaje").focus();
+$("#txtPersonaje").on("keypress",teclaPersonaje);
 //Evento del botón btnBuscar-click
 $("#btnBuscar").on("click",buscarPersonaje);
--- marvel2/main.js
+++ marvel2/main.js
@@ -1,26 +1,54 @@
-//const app= require('electron').app
-//const BrowserWindow= require('electron').BrowserWindow
-const {app,BrowserWindow} = require('electron')
+// const app = require('electron').app
+// const BrowserWindow = require('electron').BrowserWindow --Obligatoria 
+const{app,BrowserWindow} =require('electron') //esta linea es equivalente a los dos de arriba
+const electron = require('electron')
 //Ruta donde se encuentra nuestro proyecto
-const path= require('path')
+const path = require('path')
 //Ruta pero en formato URL
-const url= require('url')
+const url  = require('url')
+//Constantes para impresion en PDF
+const fs = require('fs')//sistema de archivos
+const os = require('os')//sistema operativo
+//llamada a procedimiento interno
+const ipc = electron.ipcMain
+//interfaz de comandos o terminal
+const shell =electron.shell
+//Pantalla Principal
 let PantallaPrincipal;
 
+//evento para imprimir en PDF universal, sirve en cualquier aplicacion
+
+ipc.on('print-to-pdf',function(event){
+	const pdfPath=path.join(os.tmpdir(),'print.pdf')
+	const win=BrowserWindow.fromWebContents(event.sender)
+	win.webContents.printToPDF({},function(error,data){
+		if(error) throw error
+			fs.writeFile(pdfPath,data,function(error){
+				if (error) {
+					throw error
+				}
+				shell.openExternal('file://'+pdfPath)
+			})
+	})
+})
 function muestraPantallaPrincipal(){
-PantallaPrincipal= new BrowserWindow({
-	width: 1024, 
-	height: 768
-})
-PantallaPrincipal.on('closed', function(){
-	PantallaPrincipal=null
-})
-PantallaPrincipal.loadURL(url.format({
-	pathname: path.join(__dirname,'index.html'),
-	protocol: 'file',
-	slashes: true
-}))
-PantallaPrincipal.show()
+	//es un objeto de browserWindow
+	PantallaPrincipal = new BrowserWindow({
+		width:1024,
+		height:768
+	})
+	PantallaPrincipal.on('closed',function(){
+		PantallaPrincipal = null
+	})
+	PantallaPrincipal.loadURL(url.format({
+		pathname: path.join(__dirname,'index.html'),
+		protocol: 'file', //le estamos diciendo que es un archivo de nosotros y respete las diagonales (?)
+		slashes: true
+	}))
+	PantallaPrincipal.show()
+
 }
-//La aplicacion ejecuta este evento cuando el archivo main.js se carga en memoria.
-app.on('ready', muestraPantallaPrincipal)
+
+//La aplicación se ejecuta
+//el archivo main.js se carga en memoria. 
+app.on('ready',muestraPantallaPrincipal) 
